f46bad5be25d53e725420603fc998a0d5f3eb67b
theaux42 <tbabou@student.42.fr>
2025-02-07 00:30:12 +0100

    :bug: Need to see why it crash

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,12 +6,15 @@
 /*   By: tbabou <tbabou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 10:36:26 by tbabou            #+#    #+#             */
-/*   Updated: 2025/02/03 13:13:59 by tbabou           ###   ########.fr       */
+/*   Updated: 2025/02/07 00:29:23 by tbabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 #include "PhoneBook.hpp"
+
+// Currenlty have invalid memory usage when a string is too long
+// and after SEARCH command, it segfaults.
 
 int main(void)
 {
