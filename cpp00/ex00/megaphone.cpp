9dc66a91a8f0a13ea1095462dec926062d194489
Teo <64818281+theaux42@users.noreply.github.com>
2025-03-17 11:20:04 +0100

    Update megaphone.cpp

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -10,4 +10,6 @@ int	main(int ac, char **av)
 	for (int i = 1; i < ac; i++)
 		for (int j = 0; av[i][j]; j++)
 			std::cout << (char)toupper(av[i][j]);
+    std::cout << std::endl;
+    return (0);
 }
